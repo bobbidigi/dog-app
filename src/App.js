be8823388ce9ac54c2d5f272c65f4d208256b3d4
bobbidigi <bobbidigi34@gmail.com>
2019-10-08 19:47:54 -0400

    add api

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import "./App.css";
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import {useLocalStorage} from './utils/input'
+import {useDogImages} from './utils/api' 
+
+
 
 function App(props){
 
 
-  const [breed, setBreed] = useState('husky')
-  const [images, setImages] = useState([])
-  useEffect(() =>{
-    setImages([])
-    fetchDogImages()
-  },[breed])
-
-  const handleChange = (event) => {
-    setBreed(event.target.value)
-  }
-
-  // extract this function from lifecycle event since it's used multiple times
-  const fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${breed}/images`)
-      .then(result => {
-        setImages(result.data.message)
-      })
-      .catch(error => {
-        console.log('error:', error)
-      })
-  }
-
+  const [breed, setBreed] = useLocalStorage('breed', 'husky')
+  const [count, setCount] = useLocalStorage('count', 1)
+  const [images] = useDogImages(breed, count)
 
   return (
     <>
       <h1>The Dog Website</h1>
 
-      <select value={breed} onChange={handleChange}>
+      <select value={breed} onChange={e => setBreed(e.target.value)}>
         <option value="husky">Husky</option>
         <option value="beagle">Beagle</option>
         <option value="corgi">Corgi</option>
         <option value="boxer">Boxer</option>
       </select>
+
+      <input
+        type="number"
+        placeholder="Image Count"
+        value={count}
+        onChange={e => setCount(e.target.value)}/>
 
       <div>
         {images.map((image, index) => (
---
+++ src/utils/input.js
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+
+export function useInput(initialValue){
+    const [value, setValue] = useState(initialValue)  
+    const customSetter = (newValue) => {
+      console.log("new val", newValue)
+      setValue(newValue)
+    }
+    return [value, customSetter]
+  }
+
+export function useLocalStorage(key, initialValue){
+    const [value, setValue] = useInput(()=>{
+        return window.localStorage.getItem(key) || initialValue
+    })
+    const customSetter = (newValue) => {
+        setValue(newValue)
+        window.localStorage.setItem(key, newValue)
+    }
+    return [value, customSetter]
+}  

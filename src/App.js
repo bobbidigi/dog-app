bd5eb9437992726698247ec245de8293d91242b3
bobbidigi <bobbidigi34@gmail.com>
2019-10-08 01:21:44 -0400

    refactor class to functional w/ hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-function App() {
+function App(props){
+
+
+  const [breed, setBreed] = useState('husky')
+  const [images, setImages] = useState([])
+  useEffect(() =>{
+    setImages([])
+    fetchDogImages()
+  },[breed])
+
+  const handleChange = (event) => {
+    setBreed(event.target.value)
+  }
+
+  // extract this function from lifecycle event since it's used multiple times
+  const fetchDogImages = () => {
+    axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+      .then(result => {
+        setImages(result.data.message)
+      })
+      .catch(error => {
+        console.log('error:', error)
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>The Dog Website</h1>
+
+      <select value={breed} onChange={handleChange}>
+        <option value="husky">Husky</option>
+        <option value="beagle">Beagle</option>
+        <option value="corgi">Corgi</option>
+        <option value="boxer">Boxer</option>
+      </select>
+
+      <div>
+        {images.map((image, index) => (
+          <img key={index} src={image} alt="Dog" />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default App;
